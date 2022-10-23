3b041e6813a9b2096af7d865c0807151f7aecd2f
Neil <spinal_cord@yahoo.com>
2022-10-23 14:01:41 +0100

    First Upload

---
+++ __RAM_TEST__NEW/My_settings.h
@@ -0,0 +1,15 @@
+#define PROJ_SCREENMODE MODE15
+#define PROJ_FPS 255
+
+//#define PROJ_HIGH_RAM HIGH_RAM_ON
+#define PROJ_HIGH_RAM HIGH_RAM_MUSIC
+#define PROJ_ENABLE_SFX
+#define PROJ_ENABLE_SD_MUSIC
+#define PROJ_AUD_FREQ 22050
+#define PROJ_STREAM_LOOP 1
+
+// Enabling these 2 optinos causes my code to crash on first compile but work on the second compile.
+#define PROJ_ENABLE_SOUND 1
+#define PROJ_FILE_STREAMING
+
+#define PROJ_DEVELOPER_MODE 0
